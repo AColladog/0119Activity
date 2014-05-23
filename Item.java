0c9352b75b82499d70ccc69452237769c45d7f21
Angel <ese.acg@gmail.com>
2014-05-23 12:43:30 +0200

    añadido atributo magic a Items y atributo carta a todas las Room

--- Item.java
+++ Item.java
@@ -8,16 +8,18 @@ public class Item
     private double peso;
     private String itemDescription;
     private boolean canTake;
+    private boolean magic;
 
     /**
      * Constructor for objects of class Item
      */
-    public Item(String itemDescription, double peso, boolean canTake)
+    public Item(String itemDescription, double peso, boolean canTake, boolean magic)
     {
         // initialise instance variables
         this.itemDescription = itemDescription;
         this.peso = peso;
         this.canTake = canTake;
+        this.magic = magic;
     }
 
     /**
@@ -44,5 +46,12 @@ public class Item
     
     public void printItemInfo(){        
         System.out.println("Localizada: " + getItemDescription() + " \tQue pesa: " + getPeso() + "Kg\tTransportable: " + getCanTake()); 
+    }
+    
+    /**
+     * @return si es un objeto magico, TRUE
+     */
+    public boolean getIsMagic(){
+        return magic;
     }
 }
